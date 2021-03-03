cceb262c89cb49ba40e0582819293dcfb0ec0ff2
Apenasgabs <gabers357@gmail.com>
2021-03-02 22:08:09 -0300

    :end of first class

--- exe1.c
+++ exe1.c
@@ -1,4 +1,14 @@
 
-int main(){
+#include <stdio.h>
+
+void main(){
+    int val1, val2, soma;
     
+    printf("Valor 1 ");
+    scanf("%i", &val1 );
+    printf("Valor 1 ");
+    scanf("%i", &val2 );
+    soma = val2+val1;
+    
+
 }
